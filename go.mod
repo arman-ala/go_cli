7ee1f6d3f292f01d86fbac9da78ced713a2fdd9b
Arman Ala <arman.ala.developer@gmail.com>
2023-10-04 03:08:02 +0330

    we are getting input from user and go.mod is added.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cli.com/main
+
+go 1.21.1
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	var selectedTemplate int
+	fmt.Println("Please Select your template:")
+	fmt.Printf("1- %s\n2- %s\n", "HTML and standard CSS", "HTML Boilerplate")
+	_, err := fmt.Scanln(&selectedTemplate)
+	if err != nil {
+		log.Fatalf("Error reading input: %s", err)
+	}
 }
